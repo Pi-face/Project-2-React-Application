283d6d0546a532c05a181efb4574b14a360af448
Jguzman <juan@DESKTOP-DILVCG8.localdomain>
2020-03-29 17:01:46 -0400

    got the first part of filter

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,12 +1,30 @@
 import React from 'react'
+import {useState,useEffect} from 'react'
 
 function Header(){
+
+  const [searchTerm, setSearchTerm] = React.useState("");
+ 
+
+  const handleChange = e => {
+    setSearchTerm(e.target.value);
+    
+  };
+
+  console.log('Header - [searchTerm]',searchTerm)
+
+
+
     return(
         <div className='header'>
         <h4>MHAPP</h4>
           <form>  
-             <input/>
-               <button>submit</button>
+             <input
+             type='text'
+             placeholder='Search Monster'
+             onChange={handleChange}
+             value={searchTerm}
+             />
           </form>
         </div>
     )
